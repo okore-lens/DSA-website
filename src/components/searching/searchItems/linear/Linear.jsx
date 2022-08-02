09dee7ac6adfdb8bee0eb3d47ac4aa557ae6bdb1
lens <okorelens27@gmail.com>
2022-08-02 21:00:24 +0300

    Linear search

--- src/components/searching/searchItems/linear/Linear.jsx
+++ src/components/searching/searchItems/linear/Linear.jsx
@@ -5,6 +5,7 @@ import "./Linear.scss";
 const Linear = (props) => {
   const [inputs, setInputs] = useState([]);
   const [search, setSearch] = useState(1);
+  const [results, setResults] = useState("Search a number in an array");
 
   useEffect(() => {
     setInputs(props.array);
@@ -21,25 +22,18 @@ const Linear = (props) => {
     setSearch(value);
   };
 
+  let same = [];
+
   const searchHandler = (e) => {
     e.preventDefault();
     let x = search;
     for (let i = 0; i < length; i++) {
       let el = +splited[i];
-      //   console.log(x);
-      //   console.log(el);
-
-      if ((x = el)) {
-        // console.log(i);
-        console.log("Found");
+      if (x / el === 1) {
+        same.push(i);
+        console.log(same);
+        setResults(`${x} was found at indexes ${same}`);
       }
-
-      //   if (x === +splited[i]) {
-      //     alert(`Found at index ${i}`);
-      //     return
-      //   } else {
-      //     alert("Not found");
-      //   }
     }
   };
 
@@ -48,19 +42,21 @@ const Linear = (props) => {
       <h3>Linear Search</h3>
       <p>
         We start from the beginning of the array and compare all elements with
-        our desired value X untillwe find a match and stop there.
+        our desired value X untill we find a match and stop there.
       </p>
       <form className="form">
-        <input
-          value={search}
-          onChange={formHandler}
-          placeholder="Enter digit to search"
-        />
-        <button onClick={searchHandler}>Search</button>
+        <div className="fields">
+          <input
+            value={search}
+            onChange={formHandler}
+            placeholder="Enter digit to search"
+          />
+          <button onClick={searchHandler}>Search</button>
+        </div>
       </form>
       <div className="results">
         <div className="values">{inputs} </div>
-        <div className="output">Output</div>
+        <div className="output">{results}</div>
       </div>
     </div>
   );
