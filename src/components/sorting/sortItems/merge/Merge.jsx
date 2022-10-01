52f01dd3a0fa96e2edc67c271426d95d26872b7a
lens <okorelens27@gmail.com>
2022-10-02 01:20:20 +0300

    Merge Sort

--- src/components/sorting/sortItems/merge/Merge.jsx
+++ src/components/sorting/sortItems/merge/Merge.jsx
@@ -7,49 +7,122 @@ import "./Merge.scss";
 function Merge(props) {
   const [inputs, setInputs] = useState([]);
   const [clicked, setClicked] = useState(false);
-  const [results, setResults] = useState();
+  const [results, setResults] = useState([]);
+  const [ordered, setOrdered] = useState([]);
 
   useEffect(() => {
     setInputs(props.array);
   }, [props.array]);
 
-  let mergedArray = [];
+  const splited = inputs.toString().split(",");
 
-  const mergeHandler = (a, b) => {
+  const numbers = splited.map(Number);
+
+  const mergeHandler = (arr1, arr2) => {
+    const sorted = [];
     let i = 0;
     let j = 0;
 
-    while (i < a.length) {
-      if (j !== b.length) {
-        for (j; j < b.length; j++) {
-          if (a[i] > b[j]) {
-            mergedArray.push(b[j]);
-          } else {
-            mergedArray.push(a[i]);
-          }
-        }
+    while (i < arr1.length && j < arr2.length) {
+      if (arr2[j] > arr1[i]) {
+        sorted.push(arr1[i]);
+        i++;
       } else {
-        mergedArray.push(a[i]);
-      }
-      console.log(mergedArray);
-      i++;
-      console.log(i, a.length);
-      if (i === a.length) {
-        console.log(j);
-        for (j; j < b.length; j++) {
-          mergedArray.push(b[j]);
-          console.log("Pushed", b[j]);
-        }
+        sorted.push(arr2[j]);
+        j++;
       }
     }
+    while (i < arr1.length) {
+      sorted.push(arr1[i]);
+      i++;
+    }
+    while (j < arr2.length) {
+      sorted.push(arr2[j]);
+      j++;
+    }
+
+    setOrdered(sorted);
+
+    return sorted;
+  };
+
+  const mergeSort = (arr) => {
+    if (arr.length <= 1) return arr;
+    let mid = Math.floor(arr.length / 2);
+    let left = mergeSort(arr.slice(0, mid));
+    let rigth = mergeSort(arr.slice(mid));
+
+    return mergeHandler(left, rigth);
   };
 
   const sortHandler = () => {
-    mergeHandler([4, 6, 7], [1, 2, 3, 9]);
+    const start = performance.now();
+    mergeSort(numbers);
+    const end = performance.now();
+    const time = (end - start) / 1000;
+    setResults(`${ordered + ""} :: Time taken is ${time}`);
     setClicked(true);
   };
 
-  const codeSnippet = <div></div>;
+  const codeSnippet = (
+    <div className="code-snippet">
+      <pre>
+        <code>{`
+  const mergeFunction = (arr1,arr2)=>{
+    const mergedArray = [] ;
+    let i=0;
+    let j=0;
+
+    // Only runs when both arrays have elements
+    while(i < arr1.length && j < arr2.length){
+      if(arr2[j] > arr1[i]){
+        mergedArray.push(arr1[i]);
+        i++;
+      }else{
+        mergedArray.push(arr2[j]);
+        j++;
+      }
+    }
+
+    // Once we exhaust an array we push the elements of the other array
+    // The below while statements achieve the above task
+    while( i < arr1.length){
+      mergedArray.push(arr1[i]);
+      i++;
+    }
+
+    while( j < arr2.length){
+      mergedArray.push(arr2[j]);
+      j++;
+    }
+
+    return mergedArray;
+  }
+
+  const mergeSort = (arr)=>{
+    // We set the base case...it is a recursive function
+    if(arr.length <= 1) return arr;
+    const midpoint = Math.floor(arr.length / 2 );
+
+    // We split the arrray to sub-arrays consisting of one element
+    const left = mergeSort(arr.slice(0,midpoint));
+    const right = mergeSort(arr.slice(midpoint));
+
+    // We merge the left and right sub-arrays   
+    return mergeFunction(left , right );
+  }
+
+  const arr = [4,3,5,1,7,8,2,9,11]
+  const start = perfomance.now()
+  mergeSort(arr)
+  const end = perfomance.now()
+  const time = end -start
+  console.log("Sorted array:",arr,"Time taken",time)
+
+`}</code>
+      </pre>
+    </div>
+  );
   return (
     <div className="merge">
       <h3>Quick Sort</h3>
